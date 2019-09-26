f71c627ac9cb44a3c0580bcca94ef82022a5dbb1
Văn Minh <vanminh51098@gmail.com>
2019-09-26 20:58:27 +0700

    Add project slideshow

---
+++ MySlideShow/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace MySlideShow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.SlidePic = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.picture5 = new System.Windows.Forms.PictureBox();
+            this.picture4 = new System.Windows.Forms.PictureBox();
+            this.picture3 = new System.Windows.Forms.PictureBox();
+            this.picture2 = new System.Windows.Forms.PictureBox();
+            this.picture1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SlidePic)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picture5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(797, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(52, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Slide Show";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1647, -266);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SlidePic
+            // 
+            this.SlidePic.Image = ((System.Drawing.Image)(resources.GetObject("SlidePic.Image")));
+            this.SlidePic.Location = new System.Drawing.Point(1, 108);
+            this.SlidePic.Name = "SlidePic";
+            this.SlidePic.Size = new System.Drawing.Size(797, 192);
+            this.SlidePic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.SlidePic.TabIndex = 1;
+            this.SlidePic.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.picture5);
+            this.panel2.Controls.Add(this.picture4);
+            this.panel2.Controls.Add(this.picture3);
+            this.panel2.Controls.Add(this.picture2);
+            this.panel2.Controls.Add(this.picture1);
+            this.panel2.Location = new System.Drawing.Point(1, 306);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(797, 145);
+            this.panel2.TabIndex = 2;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // picture5
+            // 
+            this.picture5.Image = ((System.Drawing.Image)(resources.GetObject("picture5.Image")));
+            this.picture5.Location = new System.Drawing.Point(656, 18);
+            this.picture5.Name = "picture5";
+            this.picture5.Size = new System.Drawing.Size(100, 114);
+            this.picture5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture5.TabIndex = 4;
+            this.picture5.TabStop = false;
+            // 
+            // picture4
+            // 
+            this.picture4.Image = ((System.Drawing.Image)(resources.GetObject("picture4.Image")));
+            this.picture4.Location = new System.Drawing.Point(509, 18);
+            this.picture4.Name = "picture4";
+            this.picture4.Size = new System.Drawing.Size(100, 114);
+            this.picture4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture4.TabIndex = 3;
+            this.picture4.TabStop = false;
+            // 
+            // picture3
+            // 
+            this.picture3.Image = ((System.Drawing.Image)(resources.GetObject("picture3.Image")));
+            this.picture3.Location = new System.Drawing.Point(337, 18);
+            this.picture3.Name = "picture3";
+            this.picture3.Size = new System.Drawing.Size(100, 114);
+            this.picture3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture3.TabIndex = 2;
+            this.picture3.TabStop = false;
+            // 
+            // picture2
+            // 
+            this.picture2.Image = ((System.Drawing.Image)(resources.GetObject("picture2.Image")));
+            this.picture2.Location = new System.Drawing.Point(184, 18);
+            this.picture2.Name = "picture2";
+            this.picture2.Size = new System.Drawing.Size(100, 114);
+            this.picture2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture2.TabIndex = 1;
+            this.picture2.TabStop = false;
+            // 
+            // picture1
+            // 
+            this.picture1.Image = ((System.Drawing.Image)(resources.GetObject("picture1.Image")));
+            this.picture1.Location = new System.Drawing.Point(39, 18);
+            this.picture1.Name = "picture1";
+            this.picture1.Size = new System.Drawing.Size(100, 114);
+            this.picture1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture1.TabIndex = 0;
+            this.picture1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.SlidePic);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SlidePic)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picture5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox SlidePic;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox picture5;
+        private System.Windows.Forms.PictureBox picture4;
+        private System.Windows.Forms.PictureBox picture3;
+        private System.Windows.Forms.PictureBox picture2;
+        private System.Windows.Forms.PictureBox picture1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MySlideShow/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySlideShow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int img = 3;
+
+        private void LoadNextImage()
+        {
+            if (img == 6)
+            {
+                img = 1;
+            }
+            int a = img - 2;
+            int b = img - 1;
+            int c = img + 1;
+            int d = img + 2;
+            if (a == 0) a = 5;
+            else if (a == -1) a = 4;
+            if (b == 0) b = 5;
+            if (c == 6) c = 1;
+            if (d == 6) d = 1;
+            else if (d == 7) d = 2;
+            SlidePic.ImageLocation = string.Format(@"Image\{0}.PNG", img);
+            picture1.ImageLocation = string.Format(@"Image\{0}.PNG", a);
+            picture2.ImageLocation = string.Format(@"Image\{0}.PNG", b);
+            picture3.ImageLocation = string.Format(@"Image\{0}.PNG", img);
+            picture4.ImageLocation = string.Format(@"Image\{0}.PNG", c);
+            picture5.ImageLocation = string.Format(@"Image\{0}.PNG", d);
+            img++;
+        }
+            private void timer1_Tick(object sender, EventArgs e)
+        {
+            LoadNextImage();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
